fc150588e15f4f183217be16c1fec029648c8427
HeroHi <mohamedmostafa4102004@gmail.com>
2025-04-08 01:22:13 +0200

    Added committees constants and committee model.

---
+++ lib/features/committes/data/models/committee.dart
@@ -0,0 +1,15 @@
+class Committee {
+  final String name;
+  final String briefDescription;
+  final String description;
+  final String iconPath;
+  final int membersCount;
+
+  const Committee({
+    required this.name,
+    required this.briefDescription,
+    required this.description,
+    required this.iconPath,
+    required this.membersCount,
+  });
+}
